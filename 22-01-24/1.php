64d795b31cd196a3500d2d5922a230f826461836
sxpritah <ismael.rguezz1@gmail.com>
2024-01-22 10:59:25 +0100

    Create 1.php

---
+++ 22-01-24/1.php
@@ -0,0 +1,31 @@
+<?php
+function calcMed($notitas) {
+    $totalNotitas = count($notitas);
+
+    if ($totalNotitas > 0) {
+        $sumaNotas = array_sum($notitas);
+        return $sumaNotas / $totalNotitas;
+    }
+
+4,5,3,2,7,1,0}
+
+function imprimirNotasMayoresMedia($notitas, $media) {
+    echo "Notas mayores que la media:\n";
+    foreach ($notitas as $nota) {
+        if ($nota > $media) {
+            echo $nota . ", ";
+        }
+    }
+}
+
+
+$notasAlumno = [4,5,3,2,7,6,0];
+$mediaNotas = calcMed($notasAlumno);
+
+
+if ($mediaNotas !== null) {
+    imprimirNotasMayoresMedia($notasAlumno, $mediaNotas);
+} else {
+    echo "No hay notas disponibles.";
+}
+?>
